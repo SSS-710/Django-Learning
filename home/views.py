200ad36f110d0afed56dea92db3b0f4a74905331
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-05-27 14:51:46 +0530

    Initial Django setup

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse("Hello, world. You are at chai aur Django Home page")
+    return render(request, 'index.html ')
+def about(request):
+    return HttpResponse("Hello, world. You are at chai aur Django About page")
+def contact(request):
+    return HttpResponse("Hello, world. You are at chai aur Django contact page")
+
