8a7a6d90544aeec843bd038ba7c3db0398d11363
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-05-30 17:04:01 +0530

    fixed django migration issue

--- sss/views.py
+++ sss/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from.models import ChaiVarity
 
 def sss(request):
+def all_chai(request):
+    chais = ChaiVarity.objects.all
+    return render(request, 'chai/all_chai.html')
     return HttpResponse("SSS Page")
