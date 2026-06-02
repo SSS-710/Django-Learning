f718fade5ac2a294cd5a71430c0879e929200362
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-06-02 16:13:06 +0530

    reltionship update

--- sss/admin.py
+++ sss/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, Store,
+ChaiCertificate
 # Register your models here.
-admin.site.register(ChaiVarity)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.modelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [ChaiReviewInline]
+
+class StoresAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varities',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+admin.site.register(ChaiVarity, ChaiVareityAdmin)
+admin.site.register(Stores, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
